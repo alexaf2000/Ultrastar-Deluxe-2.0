dfaa5cfea735f25ea5e6879613666a31c8c489cf
Alex Andrés Ferruz <alex2andres@gmail.com>
2019-08-19 22:34:29 +0200

    Begin songs page

--- pages/songs.vue
+++ pages/songs.vue
@@ -1,5 +1,7 @@
 <template>
-  
+  <div class="songs-list">
+
+  </div>
 </template>
 
 <script>
@@ -9,5 +11,10 @@ export default {
 </script>
 
 <style>
-
+.songs-list{
+    margin-top: 5%;
+    width: 350vw;
+    background: red;
+    height: 75vh;
+}
 </style>
