f2e6161367b3c675f9023ba1c4aaacd9d4db4e6d
Alex Andrés Ferruz <alex2andres@gmail.com>
2019-11-11 19:32:53 +0100

    Add song object and AddSong and RemoveSong mutations

--- store/songs.js
+++ store/songs.js
@@ -1,4 +1,21 @@
 export const state = () => ({
-    list: [],
-    song: {}
-  })
+    songsList: [],
+    song: {
+        title: null,
+        artist: null,
+        year: null,
+        folder: null,
+        cover: null,
+        audio: null,
+        video: null
+    }
+})
+
+export const mutations = {
+    addSong(state, song) {
+        state.songsList.push(song)
+    },
+    removeSong(state, { value }) {
+        state.songsList.splice(state.songsList.indexOf(value), 1)
+    }
+}
