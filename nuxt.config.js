59bf2d3795067c02faa4ace676c3ea6ec4eab726
Alex Andrés Ferruz <alex2andres@gmail.com>
2019-12-19 23:32:34 +0100

    Add JQuery

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,24 +1,32 @@
+const webpack = require("webpack");
+
 module.exports = {
-	mode: 'spa',
-	head: {title: 'ultrastar'}, // Headers of the page
-	loading: false, // Disable default loading bar
-	build: {
-		extend (config, { isDev, isClient }) {
-			if (isDev && isClient) {
-				// Run ESLint on save
-				config.module.rules.push({
-					enforce: 'pre',
-					test: /\.(js|vue)$/,
-					//loader: 'eslint-loader',
-					exclude: /(node_modules)/
-				})
-			}
-			// Extend only webpack config for client-bundle
-			if (isClient) { config.target = 'electron-renderer' }
-		}
-	},
-	dev: process.env.NODE_ENV === 'DEV',
-	css: [
-		'@/assets/css/global.css'
-	]
+    mode: 'spa',
+    head: { title: 'ultrastar' }, // Headers of the page
+    loading: false, // Disable default loading bar
+    build: {
+        vendor: ["jquery"],
+        plugins: [
+            new webpack.ProvidePlugin({
+                $: "jquery"
+            })
+        ],
+        extend(config, { isDev, isClient }) {
+            if (isDev && isClient) {
+                // Run ESLint on save
+                config.module.rules.push({
+                    enforce: 'pre',
+                    test: /\.(js|vue)$/,
+                    //loader: 'eslint-loader',
+                    exclude: /(node_modules)/
+                })
+            }
+            // Extend only webpack config for client-bundle
+            if (isClient) { config.target = 'electron-renderer' }
+        }
+    },
+    dev: process.env.NODE_ENV === 'DEV',
+    css: [
+        '@/assets/css/global.css'
+    ]
 }
